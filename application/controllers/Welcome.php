c0de387c4dea4d7cbcf7a1da747348462be6bb61
Adam <adam@softebizz.com>
2017-07-16 20:35:05 +0530

    1. getRecentphotos function declaration in flickr_model.php 2. Call getRecentphotos in controllers/Welcome.php 3. Check Result in views/welcome_message.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -8,7 +8,10 @@ class Welcome extends CI_Controller {
 		#Load flickr model
 		$this->load->model('flickr_model');
 		
+		# Call model to get recent photos from Flickr
+		$data['recentphotos'] = $this->flickr_model->getRecentphotos();
+		
 		# Load View
-		$this->load->view('welcome_message');
+		$this->load->view('welcome_message', $data);
 	}
 }
--- application/models/flickr_model.php
+++ application/models/flickr_model.php
@@ -11,7 +11,40 @@ class Flickr_model extends CI_Model {
 	
 	public function getRecentphotos() 
 	{	
+		#
+		# build the API URL to call
+		#
+
+		$params = array(
+			'api_key'	=> '3bd4375728949f7d689ae85c5072b83a',
+			'method'	=> 'flickr.photos.getRecent',
+			'format'	=> 'php_serial',
+			'per_page' => '5'
+		);
+
+		$encoded_params = array();
+
+		foreach ($params as $k => $v){
+
+			$encoded_params[] = urlencode($k).'='.urlencode($v);
+		}
+
+		#
+		# call the API and decode the response
+		#
+
+		$url = "https://api.flickr.com/services/rest/?".implode('&', $encoded_params);
+
+		$ch = curl_init(); 
+		curl_setopt($ch, CURLOPT_URL, $url); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$rsp = curl_exec($ch); 
+		curl_close($ch);
+
+		$rsp_obj = unserialize($rsp);
 		
+		return $rsp_obj;
 	}
 	
 	
